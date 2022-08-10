2ce6a3c6393aebc8a4044c14f09a96416979bb51
ardinfotech2019 <ardinfotech2019@gmail.com>
2022-08-10 08:06:51 +0530

    change router.js

--- src/router/router.js
+++ src/router/router.js
@@ -1,16 +1,6 @@
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../pages/home.vue";
-import Subjects from "../pages/subjects.vue";
-import Courses from "../pages/courses.vue";
-import Result from "../pages/result.vue";
-import Login from "../pages/login.vue";
-import Students from "../pages/students.vue";
-import Studentview from "../pages/studentview.vue";
-import Studentedit from "../pages/c.vue";
-import Location from "../pages/location.vue";
-import Settings from "../pages/settings.vue";
-import Forgot from "../pages/forgot.vue";
 
 const routes = [
   {
@@ -21,49 +11,41 @@ const routes = [
       {
         path: "/d/courses",
         name: "Courses",
-        // component: Courses,
         component: import("../pages/courses.vue"),
       },
       {
         path: "/",
         name: "Subjects",
-        // component: Subjects,
         component: import("../pages/subjects.vue"),
       },
       {
         path: "/d/result",
         name: "Result",
-        // component: Result,
         component: import("../pages/result.vue"),
       },
       {
         path: "/d/students",
         name: "Students",
-        // component: Students,
         component: import("../pages/students.vue"),
       },
       {
         path: "/d/student/:id",
         name: "Studentview",
-        // component: Studentview,
         component: import("../pages/studentview.vue"),
       },
       {
         path: "/d/student/edit/:id",
         name: "Studentedit",
-        // component: Studentedit,
         component: import("../pages/studentview.vue"),
       },
       {
         path: "/d/location",
         name: "Location",
-        // component:Location,
-        component: c,
+        component: import("../pages/location.vue"),
       },
       {
         path: "/d/settings",
         name: "Settings",
-        // component: Settings,
         component: import("../pages/settings.vue"),
       },
     ],
@@ -74,25 +56,17 @@ const routes = [
   {
     path: "/login",
     name: "Login",
-    // component: Login,
     component: import("../pages/login.vue"),
   },
   {
     path: "/forgot",
     name: "Forgot",
-    // component: Forgot,
     component: import("../pages/forgot.vue"),
   },
   {
     path: "/*",
     component: Home,
   },
-
-  // {
-  //   path: "/:pathMatch(.*)*",
-  //   name: "home",
-  //   component: Home,
-  // },
 ];
 
 const router = createRouter({
